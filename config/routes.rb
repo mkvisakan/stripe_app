caf16997d4497fca6e540323fdbdad8adbd97940
Westley Bonack <westley.bonack@gmail.com>
2014-02-21 15:56:09 -0600

    Adding controllers and views for login and account pages

--- config/routes.rb
+++ config/routes.rb
@@ -5,7 +5,8 @@ TwilioApp::Application.routes.draw do
   # See how all your routes lay out with "rake routes".
 
   # You can have the root of your site routed with "root"
-  # root 'welcome#index'
+   root 'welcome#index'
+	 get 'account' => 'account#index'
 
   # Example of regular route:
   #   get 'products/:id' => 'catalog#view'
